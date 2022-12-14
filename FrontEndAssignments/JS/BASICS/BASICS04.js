fb138ce841b73d965b07fbf781ee78076bdb0247
GBBhongal <120572877+GBBhongal@users.noreply.github.com>
2022-12-14 19:02:36 +0530

    Add files via upload

---
+++ FrontEndAssignments/JS/BASICS/BASICS04.js
@@ -0,0 +1,11 @@
+// 04.Create an object "person" with properties firstname and lastname.Display these properties using 2 different ways of accessing...
+const per={
+		first:"Aman",
+		last:"Gupta",
+	};
+//first way of accessing 
+console.log(per.last);
+console.log(per.first);
+//second way of accessing
+console.log(per["first"]);
+console.log(per["last"]);
